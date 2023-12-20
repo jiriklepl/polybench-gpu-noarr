e7a93664d47adc5b4b5a2e51c84ddd64476c1622
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2023-12-20 08:49:02 +0100

    update collect.sh

--- collect.sh
+++ collect.sh
@@ -8,9 +8,9 @@ if [ -z "$POLYBENCH_GPU_DIR" ]; then
 	POLYBENCH_GPU_DIR="$BUILD_DIR/PolyBenchGpu"
 	mkdir -p "$POLYBENCH_GPU_DIR" || exit 1
 	if [ -d "$POLYBENCH_GPU_DIR/.git" ]; then
-		( cd "$POLYBENCH_GPU_DIR" && git fetch && git checkout no-cpu && git pull ) || exit 1
+		( cd "$POLYBENCH_GPU_DIR" && git fetch && git checkout noarr-compare && git pull ) || exit 1
 	else
-		git clone --branch=no-cpu "https://github.com/jiriklepl/PolyBenchGpu.git" "$POLYBENCH_GPU_DIR" || exit 1
+		git clone --branch=noarr-compare "https://github.com/jiriklepl/PolyBenchGpu.git" "$POLYBENCH_GPU_DIR" || exit 1
 	fi
 fi
 
@@ -18,16 +18,18 @@ dirname=$(mktemp -d)
 
 trap "echo deleting $dirname; rm -rf $dirname" EXIT
 
-# ( cd "$POLYBENCH_GPU_DIR/CUDA" && srun -A kdss -p gpu-short --exclusive -wampere01 --gres=gpu bash compileCodes.sh ) || exit 1
-# ( cd . && srun -A kdss -p gpu-short --exclusive -wampere01 --gres=gpu ./build.sh ) || exit 1
+( cd "$POLYBENCH_GPU_DIR/CUDA" && srun -A kdss -p gpu-short --exclusive -wampere01 --gres=gpu bash compileCodes.sh ) || exit 1
+( cd . && srun -A kdss -p gpu-short --exclusive -wampere01 --gres=gpu ./build.sh ) || exit 1
 
 DATA_DIR="data"
 
 mkdir -p "$DATA_DIR"
 
 compare_algorithms() {
+    echo "collecting $1"
     ( srun -A kdss -p gpu-short --exclusive -wampere01 --gres=gpu ./run_noarr_algorithm.sh "Noarr" "$1" & wait ) > "$DATA_DIR/$1.log"
     ( srun -A kdss -p gpu-short --exclusive -wampere01 --gres=gpu ./run_baseline_algorithm.sh "Baseline" "$2" & wait ) >> "$DATA_DIR/$1.log"
+    echo "done"
 }
 
 compare_algorithms gemm "$POLYBENCH_GPU_DIR/CUDA/GEMM/gemm.exe"
